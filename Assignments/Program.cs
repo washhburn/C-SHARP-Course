f1aa14c67f26b08045c4825d2f6654b35efdc1e8
washhburn <sannorrman@gmail.com>
2025-10-21 13:49:41 +0300

    Add files via upload

---
+++ Assignments/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace A2ConversionInC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define string variable
+            string birthYearText = "1990";
+            Console.WriteLine("If you are born in 1990,");
+
+            //Konvertera text till heltal
+
+            int birthYear = int.Parse(birthYearText);
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - birthYear;
+
+            Console.WriteLine("then you are " + age + "years old.");
+
+        }
+    }
+}
