3fb5cf833e372148803568862a15764f487a8a88
washhburn <sannorrman@gmail.com>
2025-10-21 13:26:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace A1___Variables_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Namn
+            string name = "Sandra";
+            Console.WriteLine("Namn:" + name);
+
+            //Nummer
+            int age = 30;
+            Console.WriteLine("Ålder: " + age);
+
+            //Husdjur
+            int numberOfPets = 2;
+            Console.WriteLine("Antal husdjur:" + numberOfPets);
+
+       
+
+
+        }
+    }
+}
