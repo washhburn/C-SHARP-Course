22bbef78bd39c174d0740b8013d67292fcf1b455
Sandra Norrman <sannorrman@gmail.com>
2025-10-27 13:28:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A7___While_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Counter = 1;
+            while (Counter <= 5)
+            {
+                Console.WriteLine("Counter value is: " + Counter);
+                Counter++;
+            }
+            Console.WriteLine("The loop has finished.");
+        }
+    }
+}
