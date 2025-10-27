d8ea5d9658abf410971153ba0a849c0114d0b787
Sandra Norrman <sannorrman@gmail.com>
2025-10-27 12:20:52 +0200

    Add files via upload

---
+++ Assignments/A3 Input & Output/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3_Input_and_Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.ReadLine();
+
+        }
+    }
+}
