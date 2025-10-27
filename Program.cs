430080faff4894f744d6e741e337d9a72244c1ba
Sandra Norrman <sannorrman@gmail.com>
2025-10-27 13:25:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4___IF_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IF Statement
+            Console.Write("Enter your age: ");
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
+
+            if (age < 0 || age > 18) 
+            { 
+                Console.WriteLine("Invalid age entered.");
+
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("You are eligible to vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to vote.");
+            }
+
+
+
+        }
+    }
+}
