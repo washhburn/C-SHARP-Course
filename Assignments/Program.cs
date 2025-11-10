1678df390be1d832bfc168d43c82d505711d5866
Sandra Norrman <sannorrman@gmail.com>
2025-11-10 12:44:17 +0200

    Add files via upload

---
+++ Assignments/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A15___Logical_Error_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 5, 30, 25 };
+            int max = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+
+            Console.WriteLine($"The maximum value in the array is: {max}");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
