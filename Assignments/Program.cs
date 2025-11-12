b41f283f77ab6b3f55d8e0d93bf4540131b6c7d5
Sandra Norrman <sannorrman@gmail.com>
2025-11-12 11:20:42 +0200

    Add files via upload

---
+++ Assignments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A22___Logical_Error_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine("Sum of numbers: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
