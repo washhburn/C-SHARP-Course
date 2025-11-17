ea53c0a2d2f8b66673227a3da8b660a9f453d44a
Sandra Norrman <sannorrman@gmail.com>
2025-11-17 12:11:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysUntilChristmas
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Date of today
+            var dateToday = DateTime.Today;
+            Console.WriteLine($"Date of today is {dateToday:d}.");
+
+            //Date of Christmas Day
+            DateTime XmasDay = new DateTime(2025, 12, 25);
+
+            var daysUntilChristmas = (XmasDay - dateToday).Days;
+
+            if (dateToday == XmasDay)
+            {
+                Console.WriteLine($"Today is Christmas Day!");
+            }
+            else if (dateToday < XmasDay)
+            {
+                Console.WriteLine($"There are {daysUntilChristmas} days until Christmas!");
+
+            }
+            else
+            {
+                Console.WriteLine("Christmas has passed.");
+            }
+
+        }
+    }
+}
