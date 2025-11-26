7d8b9b3975f6baa0858c5a6cb18bb5016071abf5
Sandra Norrman <sannorrman@gmail.com>
2025-11-26 14:04:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A20___Area_of_Circle_Calculation_Task
+{
+    //Task 1: Class Circle
+    class Circle
+    {
+        //Task 2a: Property
+        public double Radius;
+
+        //Task 2b: Constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        //Method to calculate perimeter
+        public double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        //Task 2c: Method to calculate area
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Enter the radius of the circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            */
+
+            //Task 3: Create Circle instance and assign radius value
+            double radius = 1.5;
+            Circle circle = new Circle(radius);
+            
+            double perimeter = circle.CalculatePerimeter();
+            double area = circle.CalculateArea();
+
+            //Task 4: Print results
+            Console.WriteLine($"The perimeter of the circle: {perimeter:F2}");
+            Console.WriteLine($"The area of the circle: {area:F2}");
+        }
+    }
+}
